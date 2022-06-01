855a5728e4e3ff0d8d7864965cc77c9f2f1e8572
Paweł Kopel <pawel.kopel2@gmail.com>
2022-06-01 02:05:28 +0200

    Update code to java 16, fix deprecated warnings

---
+++ src/main/java/org/example/MyFipaContractNetInitiatorBehaviour.java
@@ -0,0 +1,191 @@
+/*****************************************************************
+ JADE - Java Agent DEvelopment Framework is a framework to develop multi-agent systems in compliance with the FIPA specifications.
+ Copyright (C) 2000 CSELT S.p.A.
+
+ GNU Lesser General Public License
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation,
+ version 2.1 of the License.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the
+ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA  02111-1307, USA.
+ *****************************************************************/
+
+package org.example;
+
+import jade.core.AID;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+import jade.proto.ContractNetInitiator;
+import jade.util.leap.ArrayList;
+import jade.util.leap.Iterator;
+import jade.util.leap.List;
+import org.example.ontology.Appointment;
+import org.example.ontology.MSOntology;
+import org.example.ontology.Person;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Vector;
+
+/**
+ * @author Fabio Bellifemine - CSELT S.p.A
+ * @version $Date: 2003-03-19 16:07:33 +0100 (mer, 19 mar 2003) $ $Revision: 3843 $
+ */
+
+public class MyFipaContractNetInitiatorBehaviour extends ContractNetInitiator {
+
+    private final static long TIMEOUT = 60000; // 1 minute
+    private final ACLMessage cfpMsg = new ACLMessage(ACLMessage.CFP);
+    private final Appointment pendingApp;
+    private final MeetingSchedulerAgent myAgent;
+
+
+    public MyFipaContractNetInitiatorBehaviour(MeetingSchedulerAgent a, Appointment app, List group) {
+        super(a, null);
+        myAgent = a;
+        // fill the fields of the cfp message
+        cfpMsg.setLanguage(FIPANames.ContentLanguage.FIPA_SL0);
+        cfpMsg.setOntology(MSOntology.NAME);
+        cfpMsg.setReplyByDate(new Date(System.currentTimeMillis() + TIMEOUT));
+        cfpMsg.setProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET);
+        Iterator i = group.iterator();
+        while (i.hasNext()) {
+            cfpMsg.addReceiver((AID) i.next());
+        }
+
+        try {// fill the content
+            myAgent.fillAppointment(cfpMsg, app);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+            myAgent.doDelete();
+        }
+        pendingApp = (Appointment) app.clone();
+        //reset(cfpMsg); // updates the message to be sent
+        System.out.println("myFipaContractNetInitiatorBehaviour msg:" + cfpMsg);
+    }
+
+    @Override
+    protected Vector<ACLMessage> prepareCfps(ACLMessage cfp) {
+        Vector<ACLMessage> v = new Vector<>(1);
+        v.addElement(cfpMsg);
+        return v;
+    }
+
+    @Override
+    protected void handleNotUnderstood(ACLMessage msg) {
+        System.err.println("!!! ContractNetInitiator handleNotUnderstood: " + msg.toString());
+    }
+
+    @Override
+    protected void handleOutOfSequence(ACLMessage msg) {
+        System.err.println("!!! ContractNetInitiator handleOutOfSequence: " + msg.toString());
+    }
+
+    @Override
+    protected void handleRefuse(ACLMessage msg) {
+        System.err.println("!!! ContractNetInitiator received Refuse: " + msg.toString());
+    }
+
+    @Override
+    protected void handleAllResponses(Vector proposals, Vector retMsgs) {
+
+        ACLMessage msg;
+        ArrayList acceptableDates = new ArrayList();
+        ArrayList acceptedDates;
+
+        if (proposals.size() == 0)
+            return;
+
+        Calendar c = Calendar.getInstance();
+
+        for (Iterator i = pendingApp.getAllPossibleDates(); i.hasNext(); ) {
+            c.setTime((Date) i.next());
+            acceptableDates.add(c.get(Calendar.DATE));
+
+        }
+        for (int i = 0; i < proposals.size(); i++) {
+            msg = (ACLMessage) proposals.elementAt(i);
+            if (msg.getPerformative() == ACLMessage.PROPOSE) {
+                acceptedDates = new ArrayList();
+
+                try {
+                    Appointment a = myAgent.extractAppointment(msg);
+                    for (Iterator ii = a.getAllPossibleDates(); ii.hasNext(); ) {
+                        c.setTime((Date) ii.next());
+                        Integer day = c.get(Calendar.DATE);
+                        if (acceptableDates.contains(day))
+                            acceptedDates.add(day);
+                    }
+                    acceptableDates = (ArrayList) acceptedDates.clone();
+                    if (msg.getReplyWith() != null)
+                        msg.setInReplyTo(msg.getReplyWith());
+                    msg.clearAllReceiver();
+                    msg.addReceiver(msg.getSender());
+                    msg.setSender(myAgent.getAID());
+                    retMsgs.addElement(msg);
+                } catch (FIPAException e) {
+                    e.printStackTrace();
+                }
+            } // end if "propose"
+        } // end of for proposals.size()
+
+        ACLMessage replyMsg = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+        replyMsg.setLanguage(FIPANames.ContentLanguage.FIPA_SL0);
+        replyMsg.setOntology(MSOntology.NAME);
+        if (acceptableDates.size() > 0) {
+            Date d = new Date();
+            int dateNumber = (Integer) acceptableDates.get(0);
+            c.set(Calendar.DATE, dateNumber);
+            pendingApp.setFixedDate(c.getTime());
+            try {
+                myAgent.fillAppointment(replyMsg, pendingApp);
+            } catch (FIPAException e) {
+                e.printStackTrace();
+                myAgent.doDelete();
+            }
+        } else
+            replyMsg.setPerformative(ACLMessage.REJECT_PROPOSAL);
+
+        for (int i = 0; i < retMsgs.size(); i++) {
+            ((ACLMessage) retMsgs.elementAt(i)).setPerformative(replyMsg.getPerformative());
+            ((ACLMessage) retMsgs.elementAt(i)).setContent(replyMsg.getContent());
+        }
+
+    }
+
+    @Override
+    public void handleAllResultNotifications(Vector messages) {
+        // I here receive failure or inform-done
+        ACLMessage msg;
+        boolean accepted = false;
+        Person p;
+        pendingApp.clearAllInvitedPersons();
+        for (int i = 0; i < messages.size(); i++) {
+            msg = (ACLMessage) messages.elementAt(i);
+            if (msg.getPerformative() == ACLMessage.INFORM) {
+                accepted = true;
+                p = myAgent.getPersonByAgentName(msg.getSender());
+                if (p == null)
+                    p = new Person(msg.getSender().getName(), null, null);
+                pendingApp.addInvitedPersons(p);
+            }
+        }
+        if (accepted)
+            myAgent.addMyAppointment(pendingApp);
+
+    }
+} 
+
+
+    
--- src/main/java/org/example/ontology/Appointment.java
+++ src/main/java/org/example/ontology/Appointment.java
@@ -1,25 +1,25 @@
 /*****************************************************************
-JADE - Java Agent DEvelopment Framework is a framework to develop 
-multi-agent systems in compliance with the FIPA specifications.
-Copyright (C) 2000 CSELT S.p.A. 
+ JADE - Java Agent DEvelopment Framework is a framework to develop
+ multi-agent systems in compliance with the FIPA specifications.
+ Copyright (C) 2000 CSELT S.p.A.
 
-GNU Lesser General Public License
+ GNU Lesser General Public License
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation, 
-version 2.1 of the License. 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation,
+ version 2.1 of the License.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the
-Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA  02111-1307, USA.
-*****************************************************************/
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the
+ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA  02111-1307, USA.
+ *****************************************************************/
 
 package org.example.ontology;
 
@@ -32,141 +32,133 @@ import jade.util.leap.List;
 import java.util.Date;
 
 /**
-@author Fabio Bellifemine - CSELT S.p.A
-@version $Date: 2008-10-09 14:04:02 +0200 (gio, 09 ott 2008) $ $Revision: 6051 $
-*/
+ * @author Fabio Bellifemine - CSELT S.p.A
+ * @version $Date: 2008-10-09 14:04:02 +0200 (gio, 09 ott 2008) $ $Revision: 6051 $
+ */
 
-public class Appointment implements Cloneable,Concept{
+public class Appointment implements Cloneable, Concept {
 
-    private String description = "";
+    private String description;
     private Date startingOn;
     private Date endingWith;
     private List invited = new ArrayList(); // Vector of Persons
     private List possibleDates = new ArrayList(); // Vector of Date
     private Date fixedDate;
     private AID invitingAgent;
-    
-    /**
-    * constructor
-    * @param agentName is the name of the Agent who called the appointment
-    */
+
     public Appointment() {
         startingOn = new Date();
         endingWith = new Date();
         description = "Unknown description";
     }
 
-  public void setInviter(AID name){
-    invitingAgent=name;
-  }
-
-
-  public AID getInviter(){
-    return invitingAgent;
-  }
-
-
-   public void setDescription (String descr) {
-        description = descr.replace(' ','_');
+    public AID getInviter() {
+        return invitingAgent;
     }
 
-   public String getDescription() {
+    public void setInviter(AID name) {
+        invitingAgent = name;
+    }
+
+    public String getDescription() {
         return description;
     }
 
-   public void setStartingOn( Date date) {
+    public void setDescription(String descr) {
+        description = descr.replace(' ', '_');
+    }
+
+    public Date getStartingOn() {
+        return startingOn;
+    }
+
+    public void setStartingOn(Date date) {
         startingOn = date;
     }
 
-   public Date getStartingOn() {
-        return startingOn;
-    }
-
-   public void setEndingWith (Date date) {
-        endingWith = date;
-    }
-   
-  public Date getEndingWith () {
+    public Date getEndingWith() {
         return endingWith;
     }
 
-   public  void addInvitedPersons (Person p) { 
-       invited.add(p);
+    public void setEndingWith(Date date) {
+        endingWith = date;
     }
 
-  public Iterator getAllInvitedPersons() {
-    return invited.iterator();
-  }
+    public void addInvitedPersons(Person p) {
+        invited.add(p);
+    }
 
-public void clearAllInvitedPersons() {
-  invited.clear();
-}
+    public Iterator getAllInvitedPersons() {
+        return invited.iterator();
+    }
 
-public void addPossibleDates(Date d) {
-  possibleDates.add(d);
-}
+    public void clearAllInvitedPersons() {
+        invited.clear();
+    }
 
-public Iterator getAllPossibleDates() {
-  return possibleDates.iterator();
-}
+    public void addPossibleDates(Date d) {
+        possibleDates.add(d);
+    }
 
-public void clearAllPossibleDates() {
-  possibleDates.clear();
-}
+    public Iterator getAllPossibleDates() {
+        return possibleDates.iterator();
+    }
 
-  
+    public void clearAllPossibleDates() {
+        possibleDates.clear();
+    }
+
+
     public Date getFixedDate() {
         if (fixedDate == null)
-        return startingOn;
+            return startingOn;
         else return fixedDate;
     }
 
-public void setFixedDate(Date date) {
-    fixedDate = date;
-}
-
-
-public synchronized Object clone() {
-  Appointment result;
-  try {
-    result = (Appointment)super.clone();
-    result.invited = (ArrayList)((ArrayList)invited).clone();
-    result.possibleDates = (ArrayList)((ArrayList)possibleDates).clone();
-  } catch (CloneNotSupportedException e) {
-    e.printStackTrace(); // this should never happen
-    result=null;
-  }
-  return result;
-}
-
-
-    public boolean isValid() throws Exception {
-     if (startingOn.after(endingWith))
-        throw new Exception("Ending date must be before Starting Date");
-     if (description.length() <= 0)
-        throw new Exception("The Appointment must contain a description");
-     return true;
+    public void setFixedDate(Date date) {
+        fixedDate = date;
     }
-    
-        
-    
+
+
+    public synchronized Object clone() {
+        Appointment result;
+        try {
+            result = (Appointment) super.clone();
+            result.invited = (ArrayList) ((ArrayList) invited).clone();
+            result.possibleDates = (ArrayList) ((ArrayList) possibleDates).clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace(); // this should never happen
+            result = null;
+        }
+        return result;
+    }
+
+
+    public void isValid() throws Exception {
+        if (startingOn.after(endingWith))
+            throw new Exception("Ending date must be before Starting Date");
+        if (description.length() <= 0)
+            throw new Exception("The Appointment must contain a description");
+    }
+
+
     public String toString() {
-        String str = "(Appointment ";
+        StringBuilder str = new StringBuilder("(Appointment ");
         if (description.length() > 0)
-            str = str + ":description \"" + description + "\" ";
+            str.append(":description \"").append(description).append("\" ");
         if (fixedDate == null) {
-            str = str + ":starting-on \"" + startingOn.toString() + "\" :ending-with \"" + endingWith.toString() + "\" ";
-        } else str = str + ":fixed-on \"" + fixedDate.toString() + "\" ";
-        str = str + ":invited (set ";
-        for (int i=0; i<invited.size(); i++)
-                str = str + "(" + invited.get(i).toString() + ") ";
-        str = str + ") ";
-        str = str + ":called-by " +invitingAgent.toString();
-	str = str+" :possible-dates (set ";
-	for (int i=0; i<possibleDates.size(); i++)
-	  str = str + "\"" + possibleDates.get(i).toString() + "\" ";
-	str = str + ")";
-        return str + ")";        
+            str.append(":starting-on \"").append(startingOn.toString()).append("\" :ending-with \"").append(endingWith.toString()).append("\" ");
+        } else str.append(":fixed-on \"").append(fixedDate).append("\" ");
+        str.append(":invited (set ");
+        for (int i = 0; i < invited.size(); i++)
+            str.append("(").append(invited.get(i).toString()).append(") ");
+        str.append(") ");
+        str.append(":called-by ").append(invitingAgent.toString());
+        str.append(" :possible-dates (set ");
+        for (int i = 0; i < possibleDates.size(); i++)
+            str.append("\"").append(possibleDates.get(i).toString()).append("\" ");
+        str.append(")");
+        return str + ")";
     }
-    
+
 }
